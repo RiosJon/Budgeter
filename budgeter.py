841ccf601eba013f6ceb4da449293c4ef2af46fc
Jonathan <jonathanrios77@yahoo.com>
2021-10-17 17:30:11 -0600

    Functions 3 finished and function 4 started

--- budgeter.py
+++ budgeter.py
@@ -12,18 +12,36 @@ def load_data():
   return list_of_data
 
 def view_previous_entries(entries):
-  pass
+  #The goal is to show all of the accounting entries in a human readable 
+  view_previous_entries = DATA_FILE
+  return entries
+  
 
 def display_profit_loss(entries):
-  pass
+  expenses = 0
+  income = 0
+  for row in entries:
+    if row["transaction"] == "Income":
+      income += row["amount"]
+    elif row['transaction'] == "Expenses":
+      expenses += row['amount']
+  print(expenses, ':Theses are your expenses')
+  print(income, ": This is your income")
+  print(income - expenses, "This is your profit")
 
+  
 def add_new_entry(entries):
-  pass
-
+  date = input("Date of Transaction (YYYY-MM-DD):")
+  question = input("Was this income (Y/N): ")
+  if question.upper == "Y":
+    transaction = "Income"
+  elif question.upper == "N":
+    transaction = "Expenses"
+  amount = int(input("Amount: "))
+  description = input("Describe the transaction: ")
 # ===========================================
 # =    Do Not Modify Anything Below Here    =
 # ===========================================
-
 def get_menu_choice():
   choice = None
   
